aee3c69bd5448e30e6772807479b2f18958753aa
David <david@appsaloon.be>
2021-06-01 03:04:40 +0200

    added zoom and recenter feature

--- src/scripts/mandelbrot.js
+++ src/scripts/mandelbrot.js
@@ -5,27 +5,33 @@ const mandelbrot = {
     ctx: null,
     halfWidth: 0,
     halfHeight: 0,
-    mappingFactor: 0,
-    maxIterations: 18,
-    resolution: 1,
-    offsetValueX: 0,
-    offsetValueY: 0,
+    settings: {
+        mappingFactor: 0,
+        maxIterations: 18,
+        resolution: 1.5,
+        offsetValueX: 0,
+        offsetValueY: 0,
+        escapeRadius: 2,
+    },
     init() {
         document.addEventListener('DOMContentLoaded', () => {
-            window.complex = Complex
             this.loadCanvas()
+            this.setMappingFactor()
             this.drawMandelbrot()
             this.addClickEvent()
         })
     },
     utils: {
+        report() {
+            console.log("settings: ", mandelbrot.settings);
+        },
         rgb(r, g, b) {
-            return `rgb(${r}, ${g}, ${b})`
+            return `rgb(${parseInt(r)}, ${parseInt(g)}, ${parseInt(b)})`
         },
         getColor(x, y) {
             const C = new Complex(x, y)
             let Z = new Complex(0, 0)
-            for (let i = 0; i < mandelbrot.maxIterations; ++i) {
+            for (let i = 0; i < mandelbrot.settings.maxIterations; ++i) {
                 const prevZ = Z.clone()
                 Z.pow(2)
                 if (isNaN(Z.real) || isNaN(Z.im)) {
@@ -35,8 +41,8 @@ const mandelbrot = {
                 if (Z.equals(prevZ)) {
                     return this.rgb(0, 0, 0)
                 }
-                if (Z.abs() > 2) {
-                    const rgb = (Math.pow(i, 1) / mandelbrot.maxIterations * 200) + 55
+                if (Z.abs() > mandelbrot.settings.escapeRadius) {
+                    const rgb = (i / mandelbrot.settings.maxIterations * 200) + 55
                     return this.rgb(rgb, rgb, rgb)
                 }
             }
@@ -44,16 +50,16 @@ const mandelbrot = {
         },
         getOffsetValueForAxis(axis) {
             switch (axis) {
-                case 'x': return mandelbrot.offsetValueX
-                case 'y': return mandelbrot.offsetValueY
+                case 'x': return mandelbrot.settings.offsetValueX
+                case 'y': return mandelbrot.settings.offsetValueY
                 default: throw new Error('"axis" argument is required and should be either of "x" or "y"')
             }
         },
         mapValueToCoordinate(axis, value) {
-            return mandelbrot.mappingFactor * value + this.getOffsetValueForAxis(axis);
+            return mandelbrot.settings.mappingFactor * value + this.getOffsetValueForAxis(axis);
         },
         mapCoordinateToValue(axis, coordinate) {
-            return coordinate / mandelbrot.mappingFactor + this.getOffsetValueForAxis(axis);
+            return coordinate / mandelbrot.settings.mappingFactor + this.getOffsetValueForAxis(axis);
         }
     },
     loadCanvas() {
@@ -61,10 +67,12 @@ const mandelbrot = {
         this.canvas.width = window.innerWidth
         this.canvas.height = window.innerHeight
         this.ctx = this.canvas.getContext('2d')
-        this.halfWidth = this.canvas.width / 2
-        this.halfHeight = this.canvas.height / 2
-        this.mappingFactor = Math.min(this.halfWidth, this.halfHeight) * 0.95 / 1.5
+        this.halfWidth = Math.floor(this.canvas.width / 2)
+        this.halfHeight = Math.floor(this.canvas.height / 2)
         this.ctx.translate(this.halfWidth, this.halfHeight)
+    },
+    setMappingFactor() {
+        this.settings.mappingFactor = Math.min(this.halfWidth, this.halfHeight) / this.settings.resolution
     },
     drawMandelbrot() {
         for (let x = - this.halfWidth; x < this.halfWidth; x++) {
@@ -74,13 +82,18 @@ const mandelbrot = {
                 const valueY = this.utils.mapCoordinateToValue('y', y)
                 this.ctx.fillStyle = this.utils.getColor(valueX, valueY)
                 this.ctx.fillRect(x, y, 1, 1)
+                this.ctx.fillRect(x, -y, 1, 1)
             }
         }
+        this.utils.report()
     },
     addClickEvent() {
         this.canvas.addEventListener('click', (event) => {
-            this.offsetValueX = this.utils.mapCoordinateToValue('x', event.clientX - this.halfWidth)
-            this.offsetValueY = this.utils.mapCoordinateToValue('y', event.clientY - this.halfHeight)
+            this.settings.offsetValueX = this.utils.mapCoordinateToValue('x', event.clientX - this.halfWidth)
+            this.settings.offsetValueY = this.utils.mapCoordinateToValue('y', event.clientY - this.halfHeight)
+            this.settings.resolution *= 0.1
+            this.settings.maxIterations += 50
+            this.setMappingFactor()
             this.repaint()
         })
     },
@@ -91,5 +104,6 @@ const mandelbrot = {
 }
 
 window.mandelbrot = mandelbrot
+window.complex = Complex
 
 export default mandelbrot
